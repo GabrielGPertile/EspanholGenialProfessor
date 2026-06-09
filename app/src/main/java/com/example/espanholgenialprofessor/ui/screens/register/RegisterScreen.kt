c2c1a8a0c5dbbc7c263c83679f8e8770986da510
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2026-06-09 16:23:10 -0300

    feat(register): build register screen UI

--- app/src/main/java/com/example/espanholgenialprofessor/ui/screens/register/RegisterScreen.kt
+++ app/src/main/java/com/example/espanholgenialprofessor/ui/screens/register/RegisterScreen.kt
@@ -1,13 +1,47 @@
 package com.example.espanholgenialprofessor.ui.screens.register
 
+import android.text.Layout
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
 import androidx.navigation.NavHostController
 
 @Composable
 fun RegisterScreen(
-    navController: NavHostController
+    navController: NavHostController,
+    viewModel: RegisterViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
 )
 {
-    Text("Tela do registro")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        TextField(
+            value = viewModel.email,
+            onValueChange = { viewModel.onEmailChange(it) },
+            label = { Text("email") }
+        )
+
+        TextField(
+            value = viewModel.password,
+            onValueChange = { viewModel.onPasswordChange(it) },
+            label = { Text("Senha") }
+        )
+
+        Button(
+            onClick = {
+                // depois vamos chamar Firebase aqui
+            }
+        ) {
+            Text("Criar conta")
+        }
+    }
 }
