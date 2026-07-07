0da1fdcf5e80834447fb44375f1a3597b9fa19a8
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2026-07-07 18:38:25 -0300

    feat(di): configure Hilt dependency injection

---
+++ app/src/main/java/com/example/espanholgenialprofessor/EspanholGenialProfessorApplication.kt
@@ -0,0 +1,7 @@
+package com.example.espanholgenialprofessor
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class EspanholGenialProfessorApplication : Application()
--- app/src/main/java/com/example/espanholgenialprofessor/MainActivity.kt
+++ app/src/main/java/com/example/espanholgenialprofessor/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import com.example.espanholgenialprofessor.navigation.NavGraph
 import com.example.espanholgenialprofessor.ui.theme.EspanholGenialProfessorTheme
 import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity()
 {
     override fun onCreate(savedInstanceState: Bundle?) {
